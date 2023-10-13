ddccb3fb9305780688c3e3bd2c4b4b18f1464999
Wesley Martins <wmacibnc@gmail.com>
2023-10-13 17:41:13 -0300

    Create 02-desafio.js

---
+++ 02-desafio.js
@@ -0,0 +1,25 @@
+function calcularNivel(vitorias, derrotas) {
+  const saldoVitorias = vitorias - derrotas;
+  let nivel;
+
+  if (vitorias < 10) {
+      nivel = "Ferro";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+      nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+      nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+      nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+      nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+      nivel = "Lendário";
+  } else {
+      nivel = "Imortal";
+  }
+
+  return `O Herói tem um saldo de ${saldoVitorias}, está no nível de ${nivel}`;
+}
+
+const resultado = calcularNivel(105, 20);
+console.log(resultado);
